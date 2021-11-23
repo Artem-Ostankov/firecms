595e0b0fa7fee6674b649a4026407b3f5fead2bb
Artem <arieel89@gmail.com>
2021-11-23 18:36:44 +0100

    no cms

--- src/core/FireCMS.tsx
+++ src/core/FireCMS.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DateFnsUtils from "@date-io/date-fns";
-import * as locales from "date-fns/locale";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import AdapterDateFns from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
+import DateFnsUtils from '@date-io/date-fns'
+import * as locales from 'date-fns/locale'
 
 import {
     AuthDelegate,
@@ -18,26 +18,25 @@ import {
     Navigation,
     NavigationBuilder,
     SchemaResolver,
-    StorageSource
-} from "../models";
-import { SnackbarContext, SnackbarProvider } from "./contexts/SnackbarContext";
-import { FireCMSContextProvider } from "./contexts/FireCMSContext";
-import { BreadcrumbsProvider } from "./contexts/BreacrumbsContext";
-import { ModeProvider, ModeStateContext } from "./contexts/ModeState";
-import { useBuildSideEntityController } from "./internal/useBuildSideEntityController";
-import { useBuildNavigationContext } from "./internal/useBuildNavigationContext";
+    StorageSource,
+} from '../models'
+import { SnackbarContext, SnackbarProvider } from './contexts/SnackbarContext'
+import { FireCMSContextProvider } from './contexts/FireCMSContext'
+import { BreadcrumbsProvider } from './contexts/BreacrumbsContext'
+import { ModeProvider, ModeStateContext } from './contexts/ModeState'
+import { useBuildSideEntityController } from './internal/useBuildSideEntityController'
+import { useBuildNavigationContext } from './internal/useBuildNavigationContext'
 
-import "@camberi/firecms/dist/index.css";
-import { useBuildSchemaRegistryController } from "./contexts/useBuildSchemaRegistryController";
-import { useBuildAuthController } from "./internal/useBuildAuthController";
+// import "@camberi/firecms/dist/index.css";
+import { useBuildSchemaRegistryController } from './contexts/useBuildSchemaRegistryController'
+import { useBuildAuthController } from './internal/useBuildAuthController'
 
-const DEFAULT_COLLECTION_PATH = `/c`;
+const DEFAULT_COLLECTION_PATH = `/c`
 
 /**
  * @category Core
  */
 export interface FireCMSProps<UserType> {
-
     /**
      * Use this function to return the components you want to render under
      * FireCMS
@@ -47,18 +46,18 @@ export interface FireCMSProps<UserType> {
         /**
          * Context of the app
          */
-        context: FireCMSContext;
+        context: FireCMSContext
         /**
          * Main color scheme
          */
-        mode: "dark" | "light";
+        mode: 'dark' | 'light'
         /**
          * Is one of the main processes, auth and navigation resolving, currently
          * loading. If you are building your custom implementation, you probably
          * want to show a loading indicator if this flag is `true`
          */
-        loading: boolean;
-    }) => React.ReactNode;
+        loading: boolean
+    }) => React.ReactNode
 
     /**
      * Use this prop to specify the views that will be generated in the CMS.
@@ -68,7 +67,7 @@ export interface FireCMSProps<UserType> {
      * In you need to customize the navigation based on the logged user you
      * can use a `NavigationBuilder`
      */
-    navigation: Navigation | NavigationBuilder<UserType> | EntityCollection[];
+    navigation: Navigation | NavigationBuilder<UserType> | EntityCollection[]
 
     /**
      * Do the users need to log in to access the CMS.
@@ -77,7 +76,7 @@ export interface FireCMSProps<UserType> {
      * If not specified, authentication is enabled but no user restrictions
      * apply.
      */
-    authentication?: boolean | Authenticator<UserType>;
+    authentication?: boolean | Authenticator<UserType>
 
     /**
      * Used to override schemas based on the collection path and entityId.
@@ -86,53 +85,51 @@ export interface FireCMSProps<UserType> {
      *
      * You can also override schemas in place, when using `useSideEntityController`
      */
-    schemaResolver?: SchemaResolver;
+    schemaResolver?: SchemaResolver
 
     /**
      * Format of the dates in the CMS.
      * Defaults to 'MMMM dd, yyyy, HH:mm:ss'
      */
-    dateTimeFormat?: string;
+    dateTimeFormat?: string
 
     /**
      * Locale of the CMS, currently only affecting dates
      */
-    locale?: Locale;
+    locale?: Locale
 
     /**
      * Connector to your database
      */
-    dataSource: DataSource;
+    dataSource: DataSource
 
     /**
      * Connector to your file upload/fetch implementation
      */
-    storageSource: StorageSource;
+    storageSource: StorageSource
 
     /**
      * Delegate for implementing your auth operations.
      * @see useFirebaseAuthDelegate
      */
-    authDelegate: AuthDelegate;
+    authDelegate: AuthDelegate
 
     /**
      * Optional link builder you can add to generate a button in your entity forms.
      * The function must return a URL that gets opened when the button is clicked
      */
-    entityLinkBuilder?: EntityLinkBuilder;
+    entityLinkBuilder?: EntityLinkBuilder
 
     /**
      * Default path under the navigation routes of the CMS will be created
      */
-    basePath?: string;
+    basePath?: string
 
     /**
      * Default path under the collection routes of the CMS will be created
      */
-    baseCollectionPath?: string;
-
+    baseCollectionPath?: string
 }
-
 
 /**
  * If you are using independent components of the CMS
@@ -142,7 +139,6 @@ export interface FireCMSProps<UserType> {
  * @category Core
  */
 export function FireCMS<UserType>(props: FireCMSProps<UserType>) {
-
     const {
         children,
         navigation: navigationOrBuilder,
@@ -155,21 +151,21 @@ export function FireCMS<UserType>(props: FireCMSProps<UserType>) {
         storageSource,
         dataSource,
         basePath,
-        baseCollectionPath
-    } = props;
+        baseCollectionPath,
+    } = props
 
-    const usedBasePath = basePath ?? "/";
-    const usedBasedCollectionPath = baseCollectionPath ?? DEFAULT_COLLECTION_PATH;
+    const usedBasePath = basePath ?? '/'
+    const usedBasedCollectionPath = baseCollectionPath ?? DEFAULT_COLLECTION_PATH
 
-    const dateUtilsLocale = locale ? locales[locale] : undefined;
+    const dateUtilsLocale = locale ? locales[locale] : undefined
     const authController = useBuildAuthController({
         authDelegate,
         authentication,
         dateTimeFormat,
         locale,
         dataSource,
-        storageSource
-    });
+        storageSource,
+    })
 
     const navigationContext = useBuildNavigationContext({
         basePath: usedBasePath,
@@ -179,13 +175,19 @@ export function FireCMS<UserType>(props: FireCMSProps<UserType>) {
         dateTimeFormat,
         locale,
         dataSource,
-        storageSource
-    });
+        storageSource,
+    })
 
-    const schemaRegistryController = useBuildSchemaRegistryController(navigationContext, schemaResolver);
-    const sideEntityController = useBuildSideEntityController(navigationContext, schemaRegistryController);
+    const schemaRegistryController = useBuildSchemaRegistryController(
+        navigationContext,
+        schemaResolver,
+    )
+    const sideEntityController = useBuildSideEntityController(
+        navigationContext,
+        schemaRegistryController,
+    )
 
-    const loading = authDelegate.authLoading || navigationContext.loading;
+    const loading = authDelegate.authLoading || navigationContext.loading
 
     if (navigationContext.navigationLoadingError) {
         return (
@@ -193,15 +195,14 @@ export function FireCMS<UserType>(props: FireCMSProps<UserType>) {
                 <p>There was an error while loading your navigation config</p>
                 <p>{navigationContext.navigationLoadingError}</p>
             </div>
-        );
+        )
     }
 
     return (
         <ModeProvider>
             <SnackbarProvider>
                 <SnackbarContext.Consumer>
-                    {(snackbarController) => {
-
+                    {snackbarController => {
                         const context: FireCMSContext = {
                             authController,
                             sideEntityController,
@@ -212,41 +213,35 @@ export function FireCMS<UserType>(props: FireCMSProps<UserType>) {
                             dataSource,
                             storageSource,
                             schemaRegistryController,
-                            snackbarController
-                        };
+                            snackbarController,
+                        }
 
                         return (
-                            <FireCMSContextProvider {...context} >
+                            <FireCMSContextProvider {...context}>
                                 <BreadcrumbsProvider>
                                     <LocalizationProvider
                                         dateAdapter={AdapterDateFns}
                                         utils={DateFnsUtils}
-                                        locale={dateUtilsLocale}>
+                                        locale={dateUtilsLocale}
+                                    >
                                         <DndProvider backend={HTML5Backend}>
                                             <ModeStateContext.Consumer>
                                                 {({ mode }) => {
                                                     return children({
                                                         context,
                                                         mode,
-                                                        loading
-                                                    });
+                                                        loading,
+                                                    })
                                                 }}
                                             </ModeStateContext.Consumer>
                                         </DndProvider>
                                     </LocalizationProvider>
                                 </BreadcrumbsProvider>
                             </FireCMSContextProvider>
-                        );
+                        )
                     }}
                 </SnackbarContext.Consumer>
             </SnackbarProvider>
         </ModeProvider>
-    );
-
+    )
 }
-
-
-
-
-
-
